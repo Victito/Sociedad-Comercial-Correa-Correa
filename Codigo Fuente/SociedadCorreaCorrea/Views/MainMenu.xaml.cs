6c488a9ddb1ff623dacf253ffb61503e66ee3b0b
Bruno-brrr <brun.campos@duocuc.cl>
2024-09-29 19:37:25 -0300

    Ajustes diseño cerrarbtn

--- Codigo Fuente/SociedadCorreaCorrea/Views/MainMenu.xaml.cs
+++ Codigo Fuente/SociedadCorreaCorrea/Views/MainMenu.xaml.cs
@@ -13,6 +13,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Runtime;
+using System.Windows.Interop;
+using System.Reflection.Metadata;
 
 namespace SociedadCorreaCorrea.Views
 {
@@ -24,9 +28,34 @@ namespace SociedadCorreaCorrea.Views
         public MainMenu()
         {
             InitializeComponent();
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
         }
+        [DllImport("user32.dll")]
+        private static extern IntPtr SendMessage(IntPtr hWind, int wMsg, int wParam, int lParam);
+        private void Ventana_MouseBajo(object sender, MouseButtonEventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 161, 2, 0);
+        }
+        private void panelControl_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
 
-        
-       
+
+        }
+        private void btnCerrar_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void btnMinimizar_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        private void btnMaximizar_Click(object sender, RoutedEventArgs e)
+        {
+            if(this.WindowState==WindowState.Normal)
+                this.WindowState = WindowState.Maximized;
+            else this.WindowState = WindowState.Normal;
+        }
     }
 }
