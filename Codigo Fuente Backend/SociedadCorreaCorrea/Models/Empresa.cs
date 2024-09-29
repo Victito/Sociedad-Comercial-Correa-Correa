de09f361e719dac799f139c37f7496a6fee8384b
Victito <vic.martinezs@duocuc.cl>
2024-09-29 19:34:42 -0300

    AnadidoDeBackendParaLaVistaTrabajoPendiente

---
+++ Codigo Fuente Backend/SociedadCorreaCorrea/Models/Empresa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SociedadCorreaCorrea.Models;
+
+public partial class Empresa
+{
+    public int IdEmpresa { get; set; }
+
+    public string NombreEmpresa { get; set; } = null!;
+
+    public virtual ICollection<Empleado> Empleados { get; set; } = new List<Empleado>();
+
+    public virtual ICollection<Sucursal> Sucursals { get; set; } = new List<Sucursal>();
+
+    public virtual ICollection<Usuario> Usuarios { get; set; } = new List<Usuario>();
+}
