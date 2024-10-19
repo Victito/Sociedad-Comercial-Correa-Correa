db32f691bb85d11543275ffc9366742187db13ae
3lmaxi <101214076+3lmaxi@users.noreply.github.com>
2024-10-19 15:25:59 -0400

    Update and rename sociedadcorreacorrea-actualizado8.sql to sociedadcorreacorrea.sql

---
+++ Documentacion Tecnica/sociedadcorreacorrea.sql
@@ -0,0 +1,243 @@
+﻿USE [sociedadcorreacorrea_bd]
+GO
+/****** Object:  Table [dbo].[Acuses]    Script Date: 18-10-2024 0:00:14 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Acuses](
+	[idAcuse] [int] IDENTITY(1,1) NOT NULL,
+	[nombre] [varchar](100) NOT NULL,
+	[recinto] [varchar](100) NOT NULL,
+	[fecha] [date] NOT NULL,
+	[rut] [varchar](20) NOT NULL,
+	[firma] [varchar](255) NULL,
+	[idFactura] [int] NOT NULL,
+	[idSucursal] [int] NULL,
+	[idEmpresa] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idAcuse] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Configuracion]    Script Date: 18-10-2024 0:00:14 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Configuracion](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Clave] [nvarchar](255) NOT NULL,
+	[Valor] [nvarchar](255) NOT NULL,
+	[IdEmpresa] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Empleados]    Script Date: 18-10-2024 0:00:14 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Empleados](
+	[idEmpleado] [int] IDENTITY(1,1) NOT NULL,
+	[idEmpresa] [int] NOT NULL,
+	[idSucursal] [int] NULL,
+	[idUsuario] [bigint] NULL,
+	[nombre] [varchar](100) NOT NULL,
+	[apellido] [varchar](100) NOT NULL,
+	[fechaNacimiento] [date] NULL,
+	[direccion] [varchar](255) NULL,
+	[telefono] [varchar](20) NULL,
+	[correo] [varchar](100) NULL,
+	[puesto] [varchar](100) NULL,
+	[salario] [decimal](10, 2) NULL,
+	[fechaContratacion] [date] NULL,
+	[estatus] [varchar](20) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idEmpleado] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Empresa]    Script Date: 18-10-2024 0:00:14 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Empresa](
+	[idEmpresa] [int] IDENTITY(1,1) NOT NULL,
+	[nombreEmpresa] [varchar](100) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idEmpresa] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Facturas]    Script Date: 18-10-2024 0:00:14 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Facturas](
+	[idFactura] [int] IDENTITY(1,1) NOT NULL,
+	[idUsuario] [bigint] NOT NULL,
+	[idSucursal] [int] NULL,
+	[idEmpresa] [int] NOT NULL,
+	[razon_social] [varchar](250) NULL,
+	[rut_emisor] [varchar](250) NULL,
+	[giro] [varchar](250) NULL,
+	[direccion] [varchar](250) NULL,
+	[comuna] [varchar](250) NULL,
+	[ciudad] [varchar](250) NULL,
+	[entregar_en] [varchar](250) NULL,
+	[fecha_emision] [date] NULL,
+	[fecha_vencimiento] [date] NULL,
+	[cobrador] [varchar](250) NULL,
+	[nota_venta] [int] NULL,
+	[orden_compra] [varchar](250) NULL,
+	[condiciones] [varchar](250) NULL,
+	[guia_despacho] [varchar](250) NULL,
+	[precio_unitario] [decimal](10, 2) NULL,
+	[cantidad] [int] NULL,
+	[total] [decimal](10, 2) NULL,
+	[estado] [varchar](250) NULL,
+	[rut_vendedor] [varchar](250) NULL,
+	[giro_vendedor] [varchar](250) NULL,
+	[razon_social_vendedor] [varchar](250) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idFactura] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Productos]    Script Date: 18-10-2024 0:00:14 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Productos](
+	[idProducto] [int] IDENTITY(1,1) NOT NULL,
+	[codigo_producto] [varchar](50) NOT NULL,
+	[descripcion] [varchar](255) NOT NULL,
+	[n_serie] [varchar](50) NOT NULL,
+	[cantidad] [int] NOT NULL,
+	[precio_unitario] [decimal](10, 2) NOT NULL,
+	[descuento] [int] NOT NULL,
+	[total] [decimal](10, 2) NOT NULL,
+	[idFactura] [int] NOT NULL,
+	[idSucursal] [int] NULL,
+	[idEmpresa] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idProducto] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Sucursal]    Script Date: 18-10-2024 0:00:14 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Sucursal](
+	[idSucursal] [int] IDENTITY(1,1) NOT NULL,
+	[idEmpresa] [int] NOT NULL,
+	[nombreSucursal] [varchar](100) NOT NULL,
+	[direccion] [varchar](255) NULL,
+	[ciudad] [varchar](100) NULL,
+	[pais] [varchar](100) NULL,
+	[codigoPostal] [varchar](20) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idSucursal] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Usuarios]    Script Date: 18-10-2024 0:00:14 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Usuarios](
+	[id] [bigint] IDENTITY(1,1) NOT NULL,
+	[idEmpresa] [int] NOT NULL,
+	[nombre_usuario] [varchar](255) NOT NULL,
+	[clave] [varchar](255) NOT NULL,
+	[rol] [varchar](50) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+UNIQUE NONCLUSTERED 
+(
+	[nombre_usuario] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Empleados] ADD  DEFAULT ('Activo') FOR [estatus]
+GO
+ALTER TABLE [dbo].[Facturas] ADD  DEFAULT ('Pendiente') FOR [estado]
+GO
+ALTER TABLE [dbo].[Acuses]  WITH CHECK ADD  CONSTRAINT [FK_Acuses_Factura] FOREIGN KEY([idFactura])
+REFERENCES [dbo].[Facturas] ([idFactura])
+ON UPDATE CASCADE
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[Acuses] CHECK CONSTRAINT [FK_Acuses_Factura]
+GO
+ALTER TABLE [dbo].[Configuracion]  WITH CHECK ADD  CONSTRAINT [FK_Configuracion_Empresa] FOREIGN KEY([IdEmpresa])
+REFERENCES [dbo].[Empresa] ([idEmpresa])
+GO
+ALTER TABLE [dbo].[Configuracion] CHECK CONSTRAINT [FK_Configuracion_Empresa]
+GO
+ALTER TABLE [dbo].[Empleados]  WITH CHECK ADD  CONSTRAINT [FK_Empleados_Empresa] FOREIGN KEY([idEmpresa])
+REFERENCES [dbo].[Empresa] ([idEmpresa])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[Empleados] CHECK CONSTRAINT [FK_Empleados_Empresa]
+GO
+ALTER TABLE [dbo].[Empleados]  WITH CHECK ADD  CONSTRAINT [FK_Empleados_Sucursal] FOREIGN KEY([idSucursal])
+REFERENCES [dbo].[Sucursal] ([idSucursal])
+GO
+ALTER TABLE [dbo].[Empleados] CHECK CONSTRAINT [FK_Empleados_Sucursal]
+GO
+ALTER TABLE [dbo].[Empleados]  WITH CHECK ADD  CONSTRAINT [FK_Empleados_Usuarios] FOREIGN KEY([idUsuario])
+REFERENCES [dbo].[Usuarios] ([id])
+GO
+ALTER TABLE [dbo].[Empleados] CHECK CONSTRAINT [FK_Empleados_Usuarios]
+GO
+ALTER TABLE [dbo].[Facturas]  WITH CHECK ADD  CONSTRAINT [FK_Facturas_Usuario] FOREIGN KEY([idUsuario])
+REFERENCES [dbo].[Usuarios] ([id])
+ON UPDATE CASCADE
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[Facturas] CHECK CONSTRAINT [FK_Facturas_Usuario]
+GO
+ALTER TABLE [dbo].[Productos]  WITH CHECK ADD  CONSTRAINT [FK_Productos_Factura] FOREIGN KEY([idFactura])
+REFERENCES [dbo].[Facturas] ([idFactura])
+ON UPDATE CASCADE
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[Productos] CHECK CONSTRAINT [FK_Productos_Factura]
+GO
+ALTER TABLE [dbo].[Sucursal]  WITH CHECK ADD  CONSTRAINT [FK_Sucursal_Empresa] FOREIGN KEY([idEmpresa])
+REFERENCES [dbo].[Empresa] ([idEmpresa])
+ON UPDATE CASCADE
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[Sucursal] CHECK CONSTRAINT [FK_Sucursal_Empresa]
+GO
+ALTER TABLE [dbo].[Usuarios]  WITH CHECK ADD  CONSTRAINT [FK_Usuarios_Empresa] FOREIGN KEY([idEmpresa])
+REFERENCES [dbo].[Empresa] ([idEmpresa])
+ON UPDATE CASCADE
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[Usuarios] CHECK CONSTRAINT [FK_Usuarios_Empresa]
+GO
+USE [master]
+GO
+ALTER DATABASE [sociedadcorreacorrea_bd] SET  READ_WRITE 
+GO
