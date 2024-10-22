a3f6204587d447f3dae4ad22aacd71da9f8c6346
3lmaxi <maximilianogalleguillos31@gmail.com>
2024-10-21 21:49:06 -0300

    Evidencia de aplicacion y codigo de base de datos
    Aqui se adjunta el script de la base de datos y la evidencia de la aplicacion(codigo fuente) desarrollado hasta el sprint 3

---
+++ Fase 2/Evidencias Proyecto/SociedadCorreaCorrea/Models/Empresa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SociedadCorreaCorrea.Models;
+
+public partial class Empresa
+{
+    public int IdEmpresa { get; set; }
+
+    public string NombreEmpresa { get; set; } = null!;
+
+    public virtual ICollection<Configuracion> Configuracions { get; set; } = new List<Configuracion>();
+
+    public virtual ICollection<Empleado> Empleados { get; set; } = new List<Empleado>();
+
+    public virtual ICollection<Sucursal> Sucursals { get; set; } = new List<Sucursal>();
+
+    public virtual ICollection<Usuario> Usuarios { get; set; } = new List<Usuario>();
+}
