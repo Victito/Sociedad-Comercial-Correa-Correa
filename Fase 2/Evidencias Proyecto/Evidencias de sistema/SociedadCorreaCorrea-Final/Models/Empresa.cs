d1814f04fdc4e82f2d67e12c0616c27536255277
Victito <vic.martinezs@duocuc.cl>
2024-11-25 19:05:11 -0300

    Adjuntado Evidencia proyecto fase 2 (Escritorio)

---
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/SociedadCorreaCorrea-Final/Models/Empresa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SociedadCorreaCorrea.Models;
+
+public partial class Empresa
+{
+    public int IdEmpresa { get; set; }
+
+    public string NombreEmpresa { get; set; } = null!;
+
+    public virtual ICollection<Configuracion> Configuracions { get; set; } = new List<Configuracion>();
+
+    public virtual ICollection<Empleado> Empleados { get; set; } = new List<Empleado>();
+
+    public virtual ICollection<EntradaSalidum> EntradaSalida { get; set; } = new List<EntradaSalidum>();
+
+    public virtual ICollection<Servicio> Servicios { get; set; } = new List<Servicio>();
+
+    public virtual ICollection<Sucursal> Sucursals { get; set; } = new List<Sucursal>();
+
+    public virtual ICollection<TareasDiaria> TareasDiaria { get; set; } = new List<TareasDiaria>();
+
+    public virtual ICollection<Usuario> Usuarios { get; set; } = new List<Usuario>();
+}
---
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/SociedadCorreaCorrea-Final/Models/Puesto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SociedadCorreaCorrea.Models;
+
+public partial class Puesto
+{
+    public int IdPuestos { get; set; }
+
+    public int IdEmpresa { get; set; }
+
+    public string NombrePuesto { get; set; } = null!;
+
+    public string EstadoPuesto { get; set; } = null!;
+}
